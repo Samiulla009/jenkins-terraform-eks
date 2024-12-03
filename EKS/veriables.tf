a366d101599e828d76ccfcb840444d7312842a6e
samiulla009 <shaik.samiulla009@gmail.com>
2024-12-03 23:58:22 +0530

    new file created

--- EKS/veriables.tf
+++ EKS/veriables.tf
@@ -14,3 +14,23 @@ variable "private_subnets" {
   type        = list(string)
   default     = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
 }
+
+variable "aws_key_pair_name" {
+  type    = string
+  default = "devops-testshell" # Replace with your existing key pair name
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-0dee22c13ea7a9a67" # Amazon Linux AMI (Replace with a valid AMI for your region)
+}
+
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
--- Jenkins_server/veriables.tf
+++ Jenkins_server/veriables.tf
@@ -13,3 +13,23 @@ variable "public_subnets" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
 }
+
+variable "aws_key_pair_name" {
+  type    = string
+  default = "devops-testshell" # Replace with your existing key pair name
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-0dee22c13ea7a9a67" # Amazon Linux AMI (Replace with a valid AMI for your region)
+}
+
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
